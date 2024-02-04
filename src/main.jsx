51d3cf481506204d7600a20dbf756959a2cb0794
DisonUyoga <disonobudho233@gmail.com>
2024-02-04 13:52:16 +0300

    last

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "remixicon/fonts/remixicon.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 
-axios.defaults.baseURL = "http://electronics233.pythonanywhere.com";
+axios.defaults.baseURL = "https://electronics233.pythonanywhere.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://electronics233.pythonanywhere.com",
+    baseUrl: "https://electronics233.pythonanywhere.com",
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
